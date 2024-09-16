a830b1574e28d18f2d3544fc77310c1d5822b2fc
Andrew <andrew@8ix.co.uk>
2024-09-16 12:48:02 +0100

    wip: throttling with config controlls

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products', [ProductController::class, 'index'])
+    ->middleware(
+        'throttle:' . config('throttle.products.max_attempts') . ',' . config('throttle.products.decay_minutes')
+);
+
